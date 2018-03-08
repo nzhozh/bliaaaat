3b154716f422c17e0a72b828803507667ca62411
nzhozh <37160801+nzhozh@users.noreply.github.com>
2018-03-08 03:23:56 +0300

    Update yulia_2D.m
    я программииист!

--- yulia_2D.m
+++ yulia_2D.m
@@ -1,14 +1,15 @@
+ya programmiiiiist
 load('C:\Users\natalia\Downloads\plPLspl_plPLfpl_stim.mat')
 EEG1 = pop_loadset('C:\Users\natalia\Downloads\aleskerova1.set');
 fpath_chanlocs='C:\matl\eeglab12_0_2_5b\plugins\dipfit2.2\standard_BESA\standard-10-5-cap385.elp';
 EEG1 = pop_chanedit(EEG1, 'lookup', fpath_chanlocs);
 chan_locs = EEG1.chanlocs;
-data=data{2}; %путь к TF данным (фазовоНЕсвяз)
-%ROI_freqs_chans_times={{8:13,{{'Pz'}},{0:500}},{4:7, {{'Fp1','Fp2','Fpz'}}, {0:500}}}; %для каждой частоты свои каналы и время
+data=data{2}; %ГЇГіГІГј ГЄ TF Г¤Г Г­Г­Г»Г¬ (ГґГ Г§Г®ГўГ®ГЌГ…Г±ГўГїГ§)
+%ROI_freqs_chans_times={{8:13,{{'Pz'}},{0:500}},{4:7, {{'Fp1','Fp2','Fpz'}}, {0:500}}}; %Г¤Г«Гї ГЄГ Г¦Г¤Г®Г© Г·Г Г±ГІГ®ГІГ» Г±ГўГ®ГЁ ГЄГ Г­Г Г«Г» ГЁ ГўГ°ГҐГ¬Гї
 %ROI_freqs_chans_times={{8:13,{{'O2','O1'},{'P3','P4'}},{1300:1400, 900:1000, 800:900,700:800,500:700, 400:600, 300:500, 100:400}}};
 ROI_freqs_chans_times={{8:13,{0:100, -300:0, 0:300, 100:400, 300:500, 400:600, 500:700, 600:800, 700:900, 800:1000, 900:1100, 1000:1200, 1100:1300, 1200:1400}}};
 %lastname=[1:7, 10:14, 16:25, 27:33,35:36, 38:51];
-    chel=mean(data,4); %теперь это типа 1 чел
+    chel=mean(data,4); %ГІГҐГЇГҐГ°Гј ГЅГІГ® ГІГЁГЇГ  1 Г·ГҐГ«
     for r = 1:length(ROI_freqs_chans_times)
         [h,ind]=ismember(ROI_freqs_chans_times{r}{1},freqs_tf); 
         fr=mean(chel(ind,:,:,:,:),1); 
@@ -17,7 +18,7 @@ ROI_freqs_chans_times={{8:13,{0:100, -300:0, 0:300, 100:400, 300:500, 400:600, 5
             ind(ind==0)=[];
             times=mean(fr(:,ind,:,:,:),2); 
             b=strcat(num2str(ROI_freqs_chans_times{r}{1}(1)),'-',num2str(ROI_freqs_chans_times{r}{1}(end)),'_',num2str(ROI_freqs_chans_times{r}{2}{z}(1)),'-',num2str(ROI_freqs_chans_times{r}{2}{z}(end)));
-            correlations=squeeze(times); %из-за того, что при усреднении по частоте данные не потеряли одного измерения, убираем его так
+            correlations=squeeze(times); %ГЁГ§-Г§Г  ГІГ®ГЈГ®, Г·ГІГ® ГЇГ°ГЁ ГіГ±Г°ГҐГ¤Г­ГҐГ­ГЁГЁ ГЇГ® Г·Г Г±ГІГ®ГІГҐ Г¤Г Г­Г­Г»ГҐ Г­ГҐ ГЇГ®ГІГҐГ°ГїГ«ГЁ Г®Г¤Г­Г®ГЈГ® ГЁГ§Г¬ГҐГ°ГҐГ­ГЁГї, ГіГЎГЁГ°Г ГҐГ¬ ГҐГЈГ® ГІГ ГЄ
             figure
             topoplot(correlations(:,1),chan_locs(2:28)); %correlations(:,1) - slow
             title(b)
